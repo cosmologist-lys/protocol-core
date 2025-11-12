4c77361b0ebf1d15869e4df146d2f69d3357c0b6
ptk <lyskaka22@qq.com>
2025-11-12 15:19:54 +0800

    update crc

--- src/utils/crc_util.rs
+++ src/utils/crc_util.rs
@@ -17,6 +17,21 @@ pub fn calculate_from_bytes(crc_type: CrcType, bytes: &[u8]) -> ProtocolResult<u
     crc_type.calculate(bytes)
 }
 
+pub fn calculate_from_bytes_and_collect_hex_and_bytes(
+    crc_type: CrcType,
+    bytes: &[u8],
+    swap: bool,
+) -> ProtocolResult<(String, Vec<u8>)> {
+    let res = calculate_from_bytes(crc_type, bytes)?;
+    let crc_bytes = if swap {
+        u16::to_le_bytes(res)
+    } else {
+        u16::to_be_bytes(res)
+    };
+    let hex = hex_util::bytes_to_hex(&crc_bytes)?;
+    Ok((hex, crc_bytes.into()))
+}
+
 pub fn compare_crc(crc1: &str, crc2: u16) -> ProtocolResult<()> {
     let crc1_u16 = hex_util::hex_to_u16(crc1)?;
     if crc1_u16 == crc2 {
@@ -25,7 +40,7 @@ pub fn compare_crc(crc1: &str, crc2: u16) -> ProtocolResult<()> {
         let mut temp = hex_util::hex_to_bytes(crc1)?;
         temp.reverse();
         let crc1_c = hex_util::bytes_to_hex(&temp)?;
-        let crc1_u16 = hex_util::hex_to_i32(crc1_c.as_str())?;
+        let crc1_u16 = hex_util::hex_to_u16(crc1_c.as_str())?;
         let calc_ori_crc = crc1_u16.to_u16().unwrap();
         match calc_ori_crc == crc2 {
             true => Ok(()),
